0ede646700223a60aaeee76f9aa12a468e4f9d94
Jake O'Dell <jakeodell66@gmail.com>
2024-02-19 06:06:55 -0700

    Initial commit

---
+++ app/src/main/java/com/jake_odell/northernlightsimage/MainActivity.java
@@ -0,0 +1,54 @@
+package com.jake_odell.northernlightsimage;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    AnimationDrawable lightsAnimation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ImageView imgFrame=(ImageView) findViewById(R.id.imgLights);
+        imgFrame.setBackgroundResource(R.drawable.animation);
+        lightsAnimation=(AnimationDrawable) imgFrame.getBackground();
+
+        Button button1 = (Button)findViewById(R.id.btnStart);
+        Button button2 = (Button)findViewById(R.id.btnStop);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lightsAnimation.start();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lightsAnimation.stop();
+                startActivity(new Intent(MainActivity.this, Tween.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/jake_odell/northernlightsimage/Tween.java
@@ -0,0 +1,29 @@
+package com.jake_odell.northernlightsimage;
+
+import android.os.Bundle;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Tween extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_tween);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ImageView imgRotate = (ImageView) findViewById(R.id.imgTween);
+        imgRotate.startAnimation(AnimationUtils.loadAnimation(this, R.anim.rotation));
+    }
+}
